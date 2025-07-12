92f3943a7ca8f4dd3f36114dcf4cd64cc71b7d0b
Miko-Adrian <cakipoko.exe@gmail.com>
2025-07-12 08:45:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.write("Hello, *World!* :sunglasses:")
 
 st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.title("_Streamlit_ is :red[cool] :sunglasses:")
 
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 
