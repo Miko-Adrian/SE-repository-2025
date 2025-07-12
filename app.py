81aa9319c74804e99c60e572c1af6aa792efa527
Miko-Adrian <cakipoko.exe@gmail.com>
2025-07-12 09:24:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,3 +27,17 @@ st.link_button("Go to wiki", "https://rogueblox.fandom.com/wiki/Classes")
 
 VIDEO_URL = "https://www.youtube.com/watch?v=5Q1qqvMALcM"
 st.video
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
