c7cfcb9b26da06d3e3640fe89bda37d25835d7c3
Miko-Adrian <cakipoko.exe@gmail.com>
2025-07-12 09:27:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,3 +53,14 @@ with tab2:
 with tab3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(1)
+my_bar.empty()
+
+st.success('This is a success message!', icon="✅")
